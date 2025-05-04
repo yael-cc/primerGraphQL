b5a6661020b59fba7bef4be853af54a0a5884951
yael-cc <luancuevascr@ittepic.edu.mx>
2025-05-04 02:06:55 -0700

    Actualizacion de front-end

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const http = require('http');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path'); // Asegúrate de importar 'path'
 const { typeDefs, resolvers } = require('./modules/schema.js');
 const { env, initializeFirebase } = require('./config/config.js');
 
-// Configura Express para servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Si deseas servir el archivo 'index.html' directamente cuando se accede a la raíz
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// 🔥 Inicializar Firebase ANTES de cualquier uso de DB
+// Inicializar Firebase ANTES de cualquier uso de DB
 initializeFirebase();
 
 const startServer = async () => {
   const app = express();
+
+  // Configura Express para servir archivos estáticos
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // Si deseas servir el archivo 'index.html' directamente cuando se accede a la raíz
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
   // Configuración de Apollo Server
   const server = new ApolloServer({
@@ -30,13 +31,12 @@ const startServer = async () => {
   // Aplica middleware de Apollo Server
   server.applyMiddleware({ app, path: '/graphql' });
 
-  const PORT = 3000; // Puerto por defecto
+  const PORT = process.env.PORT || 3000; // Usa el puerto del entorno o el 3000 por defecto
 
   const httpServer = http.createServer(app);
   httpServer.listen(PORT, () => {
     console.log(`Servidor GraphQL listo en http://localhost:${PORT}/graphql`);
   });
-
 };
 
 startServer();
